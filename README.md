09bbcc3008ede8667d3b779d565d54fb0a060091
ZDandsomSP <1115854107@qq.com>
2023-10-25 14:45:00 +0800

    remove redundant code and add dataset download link

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ iTransformer regards independent time series as tokens to capture multivariate c
 pip install -r requirements.txt
 ```
 
-1. The datasets can be obtained from [Google Drive]() or [Tsinghua Cloud]().
+1. The datasets can be obtained from [Google Drive](https://drive.google.com/file/d/1l51QsKvQPcqILT3DwfjCgx8Dsg2rpjot/view?usp=drive_link) or [Tsinghua Cloud](https://cloud.tsinghua.edu.cn/f/2ea5ca3d621e4e5ba36a/).
 
 2. Train and evaluate model. We provide all the above tasks under the folder ./scripts/. You can reproduce the results as the following examples:
 
--- layers/SelfAttention_Family.py
+++ layers/SelfAttention_Family.py
@@ -4,7 +4,7 @@ import numpy as np
 from math import sqrt
 from utils.masking import TriangularCausalMask, ProbMask
 from reformer_pytorch import LSHSelfAttention
-from einops import rearrange, repeat
+from einops import rearrange
 
 
 class FlowAttention(nn.Module):
@@ -39,44 +39,6 @@ class FlowAttention(nn.Module):
         x = (((queries @ kv) * normalizer_row[:, :, :, None]) * normalizer_row_refine[:, :, :, None]).transpose(1,
                                                                                                                 2).contiguous()
         return x, None
-
-
-class DSAttention(nn.Module):
-    '''De-stationary Attention'''
-
-    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
-        super(DSAttention, self).__init__()
-        self.scale = scale
-        self.mask_flag = mask_flag
-        self.output_attention = output_attention
-        self.dropout = nn.Dropout(attention_dropout)
-
-    def forward(self, queries, keys, values, attn_mask, tau=None, delta=None):
-        B, L, H, E = queries.shape
-        _, S, _, D = values.shape
-        scale = self.scale or 1. / sqrt(E)
-
-        tau = 1.0 if tau is None else tau.unsqueeze(
-            1).unsqueeze(1)  # B x 1 x 1 x 1
-        delta = 0.0 if delta is None else delta.unsqueeze(
-            1).unsqueeze(1)  # B x 1 x 1 x S
-
-        # De-stationary Attention, rescaling pre-softmax score with learned de-stationary factors
-        scores = torch.einsum("blhe,bshe->bhls", queries, keys) * tau + delta
-
-        if self.mask_flag:
-            if attn_mask is None:
-                attn_mask = TriangularCausalMask(B, L, device=queries.device)
-
-            scores.masked_fill_(attn_mask.mask, -np.inf)
-
-        A = self.dropout(torch.softmax(scale * scores, dim=-1))
-        V = torch.einsum("bhls,bshd->blhd", A, values)
-
-        if self.output_attention:
-            return (V.contiguous(), A)
-        else:
-            return (V.contiguous(), None)
 
 
 class FlashAttention(nn.Module):
@@ -364,61 +326,3 @@ class ReformerLayer(nn.Module):
         queries = self.attn(self.fit_length(queries))[:, :N, :]
         return queries, None
 
-
-class TwoStageAttentionLayer(nn.Module):
-    '''
-    The Two Stage Attention (TSA) Layer
-    input/output shape: [batch_size, Data_dim(D), Seg_num(L), d_model]
-    '''
-
-    def __init__(self, configs,
-                 seg_num, factor, d_model, n_heads, d_ff=None, dropout=0.1):
-        super(TwoStageAttentionLayer, self).__init__()
-        d_ff = d_ff or 4 * d_model
-        self.time_attention = AttentionLayer(FullAttention(False, configs.factor, attention_dropout=configs.dropout,
-                                                           output_attention=configs.output_attention), d_model, n_heads)
-        self.dim_sender = AttentionLayer(FullAttention(False, configs.factor, attention_dropout=configs.dropout,
-                                                       output_attention=configs.output_attention), d_model, n_heads)
-        self.dim_receiver = AttentionLayer(FullAttention(False, configs.factor, attention_dropout=configs.dropout,
-                                                         output_attention=configs.output_attention), d_model, n_heads)
-        self.router = nn.Parameter(torch.randn(seg_num, factor, d_model))
-
-        self.dropout = nn.Dropout(dropout)
-
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.norm3 = nn.LayerNorm(d_model)
-        self.norm4 = nn.LayerNorm(d_model)
-
-        self.MLP1 = nn.Sequential(nn.Linear(d_model, d_ff),
-                                  nn.GELU(),
-                                  nn.Linear(d_ff, d_model))
-        self.MLP2 = nn.Sequential(nn.Linear(d_model, d_ff),
-                                  nn.GELU(),
-                                  nn.Linear(d_ff, d_model))
-
-    def forward(self, x, attn_mask=None, tau=None, delta=None):
-        # Cross Time Stage: Directly apply MSA to each dimension
-        batch = x.shape[0]
-        time_in = rearrange(x, 'b ts_d seg_num d_model -> (b ts_d) seg_num d_model')
-        time_enc, attn = self.time_attention(
-            time_in, time_in, time_in, attn_mask=None, tau=None, delta=None
-        )
-        dim_in = time_in + self.dropout(time_enc)
-        dim_in = self.norm1(dim_in)
-        dim_in = dim_in + self.dropout(self.MLP1(dim_in))
-        dim_in = self.norm2(dim_in)
-
-        # Cross Dimension Stage: use a small set of learnable vectors to aggregate and distribute messages to build the D-to-D connection
-        dim_send = rearrange(dim_in, '(b ts_d) seg_num d_model -> (b seg_num) ts_d d_model', b=batch)
-        batch_router = repeat(self.router, 'seg_num factor d_model -> (repeat seg_num) factor d_model', repeat=batch)
-        dim_buffer, attn = self.dim_sender(batch_router, dim_send, dim_send, attn_mask=None, tau=None, delta=None)
-        dim_receive, attn = self.dim_receiver(dim_send, dim_buffer, dim_buffer, attn_mask=None, tau=None, delta=None)
-        dim_enc = dim_send + self.dropout(dim_receive)
-        dim_enc = self.norm3(dim_enc)
-        dim_enc = dim_enc + self.dropout(self.MLP2(dim_enc))
-        dim_enc = self.norm4(dim_enc)
-
-        final_out = rearrange(dim_enc, '(b seg_num) ts_d d_model -> b ts_d seg_num d_model', b=batch)
-
-        return final_out
