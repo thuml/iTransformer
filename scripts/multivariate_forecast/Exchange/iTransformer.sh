dc6522b328b50a161378d786229af82d4bf07b7c
ZDandsomSP <1115854107@qq.com>
2023-11-23 21:41:15 +0800

    add the Exchange dataset script

---
+++ scripts/multivariate_forecast/Exchange/iTransformer.sh
@@ -0,0 +1,80 @@
+export CUDA_VISIBLE_DEVICES=3
+
+model_name=iTransformer
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 192 \
+  --e_layers 2 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --itr 1 \
+  --d_model 128 \
+  --d_ff 128 \
+  --train_epochs 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp' \
+  --d_model 128 \
+  --d_ff 128 \
+  --itr 1
