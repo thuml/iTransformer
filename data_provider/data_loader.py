56ec65279005c9d4a200a9de356b9551994f465f
cloner174 <hamedhajipour2012@hotmail.com>
2024-05-24 01:52:52 +0330

    fix

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -191,10 +191,18 @@ class Dataset_ETT_minute(Dataset):
 
 class Dataset_Custom(Dataset):
     def __init__(self, 
-                 root_path, flag='train', size=None,
-                 features='S', data_path='data.csv',
-                 target='Close', scale=True, timeenc=0, freq='b',
-                 test_size = 0.2, kind_of_scaler = None, name_of_col_with_date = None):
+                    root_path, 
+                    flag='train', 
+                    size=None,
+                    features='MS', 
+                    data_path='data.csv',
+                    target='Close', 
+                    scale=True, 
+                    timeenc=0, 
+                    freq='b',
+                    test_size = 0.2, 
+                    kind_of_scaler = None, 
+                    name_of_col_with_date = None):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
@@ -215,7 +223,7 @@ class Dataset_Custom(Dataset):
         self.scale = scale
         self.timeenc = timeenc
         self.freq = freq
-        self.test_size = test_size
+        self.test_size = test_size if test_size is not None else 0.2
         self.train_size = 0.90 - test_size
         self.kind_of_scaler = kind_of_scaler if kind_of_scaler is not None else 'Standard'
         self.name_of_col_with_date = name_of_col_with_date if name_of_col_with_date is not None else 'date'
@@ -330,8 +338,8 @@ class Dataset_Custom(Dataset):
 
 class Dataset_PEMS(Dataset):
     def __init__(self, root_path, flag='train', size=None,
-                 features='S', data_path='ETTh1.csv',
-                 target='OT', scale=True, timeenc=0, freq='h'):
+                    features='S', data_path='ETTh1.csv',
+                    target='OT', scale=True, timeenc=0, freq='h'):
         # size [seq_len, label_len, pred_len]
         # info
         self.seq_len = size[0]
@@ -473,15 +481,15 @@ class Dataset_Solar(Dataset):
 
 class Dataset_Pred(Dataset):
     def __init__(self, root_path, flag='pred', size=None,
-                 features='S', data_path='data.csv',
-                 target='Close', scale=True, inverse=True, timeenc=0, freq='b', cols=None, 
-                 test_size = None, kind_of_scaler = None, name_of_col_with_date = None):
+                    features='S', data_path='data.csv',
+                    target='Close', scale=True, inverse=False, timeenc=0, freq='b', cols=None, 
+                    test_size = None, kind_of_scaler = None, name_of_col_with_date = None):
         # size [seq_len, label_len, pred_len]
         # info
         if size == None:
             self.seq_len = 1 * 5 * 6
             self.label_len = 1 * 1
-            self.pred_len = 1 * 1
+            self.pred_len = 1 * 3
         else:
             self.seq_len = size[0]
             self.label_len = size[1]
@@ -489,7 +497,7 @@ class Dataset_Pred(Dataset):
         # init
         assert flag in ['pred']
         
-        self.test_size = None
+        self.test_size = test_size
         self.kind_of_scaler = kind_of_scaler if kind_of_scaler is not None else 'Standard'
         self.name_of_col_with_date = name_of_col_with_date if name_of_col_with_date is not None else 'date'
         self.features = features
@@ -502,11 +510,12 @@ class Dataset_Pred(Dataset):
         self.root_path = root_path
         self.data_path = data_path
         self.__read_data__()
-
+    
+    
     def __read_data__(self):
         self.scaler = StandardScaler()
         df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+                                            self.data_path))
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
@@ -523,13 +532,13 @@ class Dataset_Pred(Dataset):
         df_raw = df_raw.set_axis(cols, axis=1)
         border1 = len(df_raw) - self.seq_len
         border2 = len(df_raw)
-
+        
         if self.features == 'M' or self.features == 'MS':
             cols_data = df_raw.columns[1:]
             df_data = df_raw[cols_data]
         elif self.features == 'S':
             df_data = df_raw[[self.target]]
-
+        
         if self.scale:
             if self.features == 'S' or self.features == 'MS':
                 col_scaled = []
@@ -567,11 +576,11 @@ class Dataset_Pred(Dataset):
                 data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-
+        
         tmp_stamp = df_raw[['date']][border1:border2]
         tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
         pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len + 1, freq=self.freq)
-
+        
         df_stamp = pd.DataFrame(columns=['date'])
         df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
         if self.timeenc == 0:
@@ -585,20 +594,20 @@ class Dataset_Pred(Dataset):
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
-
+        
         self.data_x = data[border1:border2]
         if self.inverse:
             self.data_y = df_data.values[border1:border2]
         else:
             self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
-
+    
     def __getitem__(self, index):
         s_begin = index
         s_end = s_begin + self.seq_len
         r_begin = s_end - self.label_len
         r_end = r_begin + self.label_len + self.pred_len
-
+        
         seq_x = self.data_x[s_begin:s_end]
         if self.inverse:
             seq_y = self.data_x[r_begin:r_begin + self.label_len]
@@ -606,11 +615,11 @@ class Dataset_Pred(Dataset):
             seq_y = self.data_y[r_begin:r_begin + self.label_len]
         seq_x_mark = self.data_stamp[s_begin:s_end]
         seq_y_mark = self.data_stamp[r_begin:r_end]
-
+        
         return seq_x, seq_y, seq_x_mark, seq_y_mark
-
+    
     def __len__(self):
         return len(self.data_x) - self.seq_len + 1
-
+    
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
