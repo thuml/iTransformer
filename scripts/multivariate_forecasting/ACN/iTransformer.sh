7383ac12349f729acf21e191cda582e456a5db53
Tom <tom.medebach@web.de>
2024-06-06 21:34:13 +0200

    ACN iTransformer config

---
+++ scripts/multivariate_forecasting/ACN/iTransformer.sh
@@ -0,0 +1,88 @@
+export CUDA_VISIBLE_DEVICES=2
+
+model_name=iTransformer
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/ACN/ \
+  --data_path acn.csv \
+  --model_id ACN_96_96 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 96 \
+  --e_layers 4 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 512\
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/ACN/ \
+  --data_path acn.csv \
+  --model_id ACN_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 192 \
+  --e_layers 4 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/ACN/ \
+  --data_path acn.csv \
+  --model_id ACN_96_336 \
+  --model_id traffic_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 336 \
+  --e_layers 4 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 512\
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --itr 1
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./dataset/ACN/ \
+  --data_path acn.csv \
+  --model_id ACN_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --pred_len 720 \
+  --e_layers 4 \
+  --enc_in 862 \
+  --dec_in 862 \
+  --c_out 862 \
+  --des 'Exp' \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001\
+  --itr 1
