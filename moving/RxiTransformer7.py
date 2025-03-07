e592b2fad91394ffa7189d8026f9779298c6ca59
HirokiKaneko1124 <damochi1124@gmail.com>
2025-03-07 18:18:39 +0900

    Add files via upload

---
+++ moving/RxiTransformer7.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.Transformer_EncDec import Encoder, EncoderLayer
+from layers.SelfAttention_Family import FullAttention, AttentionLayer
+from layers.Embed import DataEmbedding_inverted
+import numpy as np
+from layers.Invertible import RevIN
+
+
+class Model(nn.Module):
+    # Parrarel model of iTransformer and RLinear-CI
+
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.output_attention = configs.output_attention
+        self.use_norm = configs.use_norm
+        self.enc_in = configs.enc_in
+
+        # iTransoformer
+        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.embed, configs.freq,
+                                                    configs.dropout)
+        self.class_strategy = configs.class_strategy
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(False, configs.factor, attention_dropout=configs.dropout,
+                                      output_attention=configs.output_attention), configs.d_model, configs.n_heads),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation
+                ) for l in range(configs.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        )
+        self.projector = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+
+        # RLinear-CI
+        self.Linear = nn.ModuleList([
+            nn.Linear(configs.seq_len, configs.pred_len) for _ in range(configs.enc_in)
+        ])
+        self.dropout = nn.Dropout(configs.dropout)
+        self.rev = RevIN(configs.enc_in) if configs.rev else None
+
+        # DistributionLayer
+        distributer = []
+        for i in range(configs.enc_in):
+            distributer.append(0.5)
+        self.distributer = nn.Parameter(torch.tensor(distributer))
+
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        # Saving x_enc
+        x_enc_save = x_enc
+        
+        # Normalization
+        x_normed = self.rev(x_enc_save, 'norm') if self.rev else x_enc_save
+
+        # iTransformer
+        _, _, N = x_enc.shape 
+        enc_out = self.enc_embedding(x_normed, x_mark_enc)
+        enc_out, attns = self.encoder(enc_out, attn_mask=None)
+        enc_out = self.projector(enc_out).permute(0, 2, 1)[:, :, :N]
+        
+        # RLinear-CI
+        x = self.dropout(x_normed)
+        rl_out = torch.zeros(x.size(0), self.pred_len, x.size(2)).to('cuda')
+        for idx, proj in enumerate(self.Linear):
+           rl_out[:, :, idx] = proj(x[:, :, idx])
+        
+        # Add
+        out = torch.zeros_like(enc_out)
+        for i in range(self.enc_in):
+            rate = self.distributer[i]
+            out[:, :, i] = enc_out[:, :, i]*rate + rl_out[:, :, i]*(1 - rate)
+        
+        # Denormalization
+        out = self.rev(out, 'denorm') if self.rev else out
+
+
+        return out
+
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+        dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+        return dec_out[:, -self.pred_len:, :]  # [B, L, D]
